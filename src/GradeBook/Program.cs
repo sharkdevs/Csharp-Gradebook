bc933fe4d8e40e5a54cc141cd4faa861de7997bd
sharkdevs <mesharkz1@gmail.com>
2020-01-07 11:29:30 +0300

    bootstrap: create a project
    - create a console csharp project
    - add code for a gradebook
    
    [end]

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+       static void Main(string[] args){
+           
+        List<double> marks =  new List<double>(){3.4, 2, 4.5};
+        double result = 0;
+        double average = 0;
+        marks.Add(5.1);
+        
+        foreach (var mark in marks)
+        {
+            result += mark;
+        }
+
+        average = result/marks.Count;
+        Console.WriteLine(result +"  "+ average);
+       }
+    }
+}
